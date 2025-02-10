4e708d326528a386a8e9f74671a0a98059b922c0
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-02-10 14:22:39 +0530

    [Rakshit] Refactor: add email phone password validation

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -2,7 +2,11 @@ import Joi from '@hapi/joi';
 
 export const newUserValidator = (req, res, next) => {
   const schema = Joi.object({
-    name: Joi.string().min(4).required()
+    name: Joi.string().min(4).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().pattern(/^[0-9]{10}$/).optional(),
+    password: Joi.string().pattern(new RegExp('^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{8,})')).required()
+
   });
   const { error, value } = schema.validate(req.body);
   if (error) {
