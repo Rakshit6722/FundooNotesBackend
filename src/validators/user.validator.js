598df55292ec69c6c46aa413cb22dcf2f9e28a9c
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-02-10 15:37:56 +0530

    [Rakshit] Refactor: custom erro message with valiator

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -3,16 +3,28 @@ import Joi from '@hapi/joi';
 export const newUserValidator = (req, res, next) => {
   const schema = Joi.object({
     name: Joi.string().min(4).required(),
-    email: Joi.string().email().required(),
-    phone: Joi.string().pattern(/^[0-9]{10}$/).optional(),
-    password: Joi.string().pattern(new RegExp('^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{8,})')).required()
 
+    email: Joi.string()
+      .pattern(/^[a-zA-Z0-9._%+-]+@gmail\.com$/)
+      .required()
+      .messages({ "string.pattern.base": "Email must be a valid Gmail address" }),
+
+    phone: Joi.string().pattern(/^[0-9]{10}$/).optional(),
+
+    password: Joi.string()
+      .pattern(new RegExp('^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{8,})'))
+      .required()
+      .messages({
+        "string.pattern.base": "Password must contain at least one uppercase letter, one special character, and must be at least 8 characters long"
+      }),
   });
+
   const { error, value } = schema.validate(req.body);
+
   if (error) {
-    next(error);
-  } else {
-    req.validatedBody = value;
-    next();
+    return res.status(400).json({ error: error.details[0].message });
   }
+
+  req.validatedBody = value;
+  next();
 };
